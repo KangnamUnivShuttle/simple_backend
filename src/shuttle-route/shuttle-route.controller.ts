a77b3f17edb94bb5f9e286e0410fbfeaa15b553d
Kim HyunWoo <stories2@naver.com>
2022-09-26 00:30:56 +0900

    Update shuttle-route.controller.ts

--- src/shuttle-route/shuttle-route.controller.ts
+++ src/shuttle-route/shuttle-route.controller.ts
@@ -10,8 +10,18 @@ export class ShuttleRouteController {
     return this.shuttleRouteService.sheetTitle;
   }
 
+  @Get('routes')
+  getRoutes() {
+    return this.shuttleRouteService.scheduleHeaders;
+  }
+
   @Get('schedule/:route')
   getRouteSchedule(@Param('route') routeName: string) {
     return this.shuttleRouteService.getRouteSchedule(routeName);
+  }
+
+  @Get('fastestbustime/:route')
+  getFastestBusTimeFromRoute(@Param('route') routeName: string) {
+    return '';
   }
 }
